29b94cc0499af85565b7e780ed67d3cc8f228860
Abhiicode <abhiabhii2000@gmail.com>
2023-02-12 15:21:05 +0530

    added a change

--- src/Components/Packages/Package.jsx
+++ src/Components/Packages/Package.jsx
@@ -8,12 +8,12 @@ const Package = () => {
                 <h1>Popular Tour Destination In India</h1>
                 <p>Packages for Best-Selling Destinations in India </p>
                 <div className="packages_container">
-                    <PackageCard img_url="../../../public/images/rajasthan-tour.jpg" img_heading="Rajasthan Tour Packages" img_desc="Given a chance to time travel and relish the magical expanse of Royal Rajasthan..." />
-                    <PackageCard img_url="../../../public/images/jal-mahla.png" img_heading="Golden Triangle Tour" img_desc="Get mesmerized by the beauty of the most fascinating Indian cities (Delhi-Agra-Jaipur)" />
-                    <PackageCard img_url="../../../public/images/kashmir-tour.jpg" img_heading="Kerala Tour Packages" img_desc="Planning for a Kerala trip? Here are our most popular and trending Kerala tour packages!" />
-                    <PackageCard img_url="../../../public/images/himachal-tour.jpg" img_heading="Himachal Tour Packages" img_desc="India is famed for its rich and abundant wilderness, and wildlife sighting is a must when on a trip..." />
-                    <PackageCard img_url="../../../public/images/kashmir-tour.jpg" img_heading="Jammu And Kashmir Tour" img_desc="Are you delighted for your honeymoon vacation? This is one such escape you will treasure..." />
-                    <PackageCard img_url="../../../public/images/gujrat-tour.jpg" img_heading="Gujarat Tour Packages" img_desc="Gujarat is one of the most adorable state on the western coast of India is the sixth largest state..." />
+                    <PackageCard img_url="./images/rajasthan-tour.jpg" img_heading="Rajasthan Tour Packages" img_desc="Given a chance to time travel and relish the magical expanse of Royal Rajasthan..." />
+                    <PackageCard img_url="./images/jal-mahla.png" img_heading="Golden Triangle Tour" img_desc="Get mesmerized by the beauty of the most fascinating Indian cities (Delhi-Agra-Jaipur)" />
+                    <PackageCard img_url="./images/kashmir-tour.jpg" img_heading="Kerala Tour Packages" img_desc="Planning for a Kerala trip? Here are our most popular and trending Kerala tour packages!" />
+                    <PackageCard img_url="./images/himachal-tour.jpg" img_heading="Himachal Tour Packages" img_desc="India is famed for its rich and abundant wilderness, and wildlife sighting is a must when on a trip..." />
+                    <PackageCard img_url="./images/kashmir-tour.jpg" img_heading="Jammu And Kashmir Tour" img_desc="Are you delighted for your honeymoon vacation? This is one such escape you will treasure..." />
+                    <PackageCard img_url="./images/gujrat-tour.jpg" img_heading="Gujarat Tour Packages" img_desc="Gujarat is one of the most adorable state on the western coast of India is the sixth largest state..." />
                 </div>
             </div>
         </>
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import App from './App'
 import Navbar from './Components/Navbar/Navbar'
 import Slider from './Components/Slider/Slider'
 import Package from './Components/Packages/Package'
+import About from './Components/About/About'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar/>
     <Slider/> 
     <Package/>
-    
+    <About/>
   </React.StrictMode>,
 )
